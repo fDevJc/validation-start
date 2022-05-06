1f3d43c72f23e3bb448b648c4f26b3a89353ebb0
fDevJc <fdevjc@gmail.com>
2022-05-06 12:00:15 +0900

    [feat] Form 전송 객체 분리 마무리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,22 +9,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-//기능이 너무 약해서 권장하지는 않는다. 그냥 자바 코드로 작성하는것을 권장한다
-//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합은 10000원 이상")
 public class Item {
-
-    @NotNull(groups = UpdateCheck.class)
     private Long id;
-
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000 ,groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
